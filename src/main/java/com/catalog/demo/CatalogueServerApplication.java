cf95cc823517de8441c4bccbfff59d20ccacdb5f
Shivam Tiwari <Shivamtiwari0447.com>
2022-09-13 16:19:33 +0530

    server skeleton

---
+++ src/main/java/com/catalog/demo/CatalogueServerApplication.java
@@ -0,0 +1,13 @@
+package com.catalog.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueServerApplication.class, args);
+	}
+
+}
